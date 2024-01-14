73767ee50c67fa7a22b250a4e2520f7060e198bc
torrigreen <tgreen14@tulane.edu>
2024-01-14 13:15:41 -0600

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const FACTOR = 10;
+
+const convertToCelsius = function(temperature) {
+  return Math.round(((temperature - 32) * (5 / 9)) * FACTOR) / FACTOR;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return Math.round((temperature * (9 / 5) + 32) * FACTOR) / FACTOR;
 };
 
 // Do not edit below this line
