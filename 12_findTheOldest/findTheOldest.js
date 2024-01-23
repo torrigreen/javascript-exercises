5a24ee45a822c1a31c7862c0ab1a4e3f6a8bb22b
torrigreen <tgreen14@tulane.edu>
2024-01-23 15:44:22 -0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+	return array.reduce((oldest, current) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+		return oldestAge > currentAge ? oldest : current;
+	});
 };
+
+const getAge = (birth, death) => {
+	if (!death) death = (new Date()).getFullYear();
+	return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
