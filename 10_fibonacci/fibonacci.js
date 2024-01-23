45a495dd998a76ad7205d2a06ac52cb394282c64
torrigreen <tgreen14@tulane.edu>
2024-01-23 15:25:30 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	n = +n;
+	if (n < 0) return `OOPS`;
+	if (n === 0 || n === 1) return n;
 
-};
+
+	const fib = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	return fib[n];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
