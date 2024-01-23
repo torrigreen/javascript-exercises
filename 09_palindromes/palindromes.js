b7a2b21abd4b17819aa835b56691d9d852051c64
torrigreen <tgreen14@tulane.edu>
2024-01-23 15:15:17 -0600

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	const newString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+	return newString === newString.split('').reverse().join('');
 };
 
 // Do not edit below this line
